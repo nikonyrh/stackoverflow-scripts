4df562baa1af348ee678cecf39ea225f1defaebd
nikonyrh <niko.nyrhila@gmail.com>
2019-03-05 22:22:07 +0200

    Tabs in a .py file :o

--- 32785803_es_image_phash/py/generate_to_file.py
+++ 32785803_es_image_phash/py/generate_to_file.py
@@ -3,7 +3,6 @@ from hashlib import sha1
 import random
 import json
 import time
-import sys
 
 dim_in   = 32
 dim_rand = 25  # Number of "clusters" is about 2^(dim_in - dim_rand)
@@ -23,40 +22,42 @@ proj[0:dim_rand,:] *= rand_scale
 np.random.seed(seed=int(time.time()))
 
 def get_sampler(n_samples, bits_per_sample):
-	result = np.zeros((dim_out,bits_per_sample,n_samples), dtype=np.uint64)
-	hash   = lambda i, j: sha1(
-		('%d-%d' % (i, j)).encode('utf-8')
-	).hexdigest()
-	
-	ind2 = list(range(bits_per_sample))
-	
-	for i in range(n_samples):
-		ind1 = [(j, hash(i, j)) for j in range(dim_out)]
-		ind1.sort(key=lambda j: j[1])
-		ind1 = sorted([j[0] for j in ind1[0:bits_per_sample]])
-		
-		result[ind1,ind2,i] = 1;
-	
-	# Ah damn, maybe this should have been in a reverse order :P
-	# But I am too lazy to re-generate all data to ES
-	pow2 = 2**np.arange(bits_per_sample).astype(np.uint64)
-	return result, pow2
+    result = np.zeros((dim_out,bits_per_sample,n_samples), dtype=np.uint64)
+    hash   = lambda i, j: sha1(
+        ('%d-%d' % (i, j)).encode('utf-8')
+    ).hexdigest()
+    
+    ind2 = list(range(bits_per_sample))
+    
+    for i in range(n_samples):
+        ind1 = [(j, hash(i, j)) for j in range(dim_out)]
+        ind1.sort(key=lambda j: j[1])
+        ind1 = sorted([j[0] for j in ind1[0:bits_per_sample]])
+        
+        result[ind1,ind2,i] = 1;
+    
+    # Ah damn, maybe this should have been in a reverse order :P
+    # But I am too lazy to re-generate all data to ES
+    pow2 = 2**np.arange(bits_per_sample).astype(np.uint64)
+    return result, pow2
 
 
-def main():
-	n_out   = int(100e6)
-	n_batch = 10000
-	
-	sampler, pow2 = get_sampler(n_samples, b_p_sample)
-	
-	with open('numbers.txt', 'w') as f:
-		for i in range(n_out // n_batch):
-			data = np.random.randn(n_batch, dim_in)
-			hash = (data.dot(proj) > 0).astype(np.uint64)
-			hash_int = hash.dot(2**np.arange(dim_out).astype(np.uint64))
-			
-			f.write('\n'.join('%d' % j for j in hash_int) + '\n')
+def main(n_out):
+    n_batch = min(10000, n_out)
+    assert n_out % n_batch == 0
+
+    sampler, pow2 = get_sampler(n_samples, b_p_sample)
+    
+    with open('numbers.txt', 'w') as f:
+        for i in range(n_out // n_batch):
+            data = np.random.randn(n_batch, dim_in)
+            hash = (data.dot(proj) > 0).astype(np.uint64)
+            hash_int = hash.dot(2**np.arange(dim_out).astype(np.uint64))
+            
+            f.write('\n'.join('%d' % j for j in hash_int) + '\n')
 
 
 if __name__ == '__main__':
-	main()
+    import sys
+    main(int(sys.argv[1]) * 1000)
+
