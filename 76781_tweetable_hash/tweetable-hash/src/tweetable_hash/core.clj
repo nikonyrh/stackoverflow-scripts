2273a7f71b70e4267a1eb9763086729603703eb8
nikonyrh <niko.nyrhila@gmail.com>
2019-03-05 22:21:22 +0200

    76781 Tweetable hash function challenge (codegolf), tweaks

--- 76781_tweetable_hash/tweetable-hash/src/tweetable_hash/core.clj
+++ 76781_tweetable_hash/tweetable-hash/src/tweetable_hash/core.clj
@@ -9,13 +9,19 @@
        (filter #(.isProbablePrime (BigInteger/valueOf %) 7))
        vec))
 
+(def big-primes
+  (->> (range (- 16rffffff 100) 16777213 2)
+       (filter #(.isProbablePrime (BigInteger/valueOf %) 7))
+       (concat [16rffffff])
+       vec))
+
 (comment
   ; Best found parameters for these few distinct function constructs.
   (def f #(reduce(fn[r i](mod(+(* r 23)i)16777213))(map *(cycle(map int"ZtabAR%H|-KrykQn{]u9f:F}v#OI^so3$x54z2&gwX<S~"))(for[c %](bit-xor(int c)3))))) ; 6148
   (def f #(reduce(fn[r i](mod(+(* r 263)i)16777213))(map *(cycle(map int"i@%(J|IXt3&R5K'XOoa+Qk})w<!w[|3MJyZ!=HGzowQlN"))(map int %)(rest(range)))))   ; 6021
   (def f #(reduce(fn[r i](mod(+(* r 811)i)16777213))(map *(cycle(map int"~:XrBaXYOt3'tH-x^W?-5r:c+l*#*-dtR7WYxr(CZ,R6J7=~vk"))(map int %))))           ; 6019
   
-  (let [rows (->> "C:\\nikon\\projects\\stackoverflow-scripts\\76781_tweetable_hash\\tweetable-hash\\english.txt" slurp (re-seq #"[^\r\n]+"))]
+  (let [rows (->> "/home/wrecked/projects/stackoverflow-scripts/76781_tweetable_hash/tweetable-hash/english.txt" slurp (re-seq #"[^\r\n]+"))]
     (for [limit [8 12 16 50]]
       [limit
        (->> rows
@@ -31,15 +37,16 @@
   ; The idea is to first find optimal prefix for the seed so that words shorter than N
   ; characters have the least number of collisions. Later we'll advance  to process
   ; longer and longer strings, hoping to find a good seed value.
-  (let [f         #(reduce(fn[r i](mod(+(* r %)i)16777213))(map *(cycle(map int %2))(map int %3)))
+  (let [f         #(reduce(fn[r i](mod(+(* r %)i)%2))(map *(cycle(map int %3))(map int %4)))
         best-score (atom 1e6)
         rows    (->> fname slurp (re-seq #"[^\r\n]+") (filter #(<= (count %) word-len))
                      (sort-by count) vec)
         gen-len (- word-len (count seed))
         params  (for [c [(->> (range 32 127) (map char) (remove (set "\\`\"\n")) vec)]
-                      rand-char [#(rand-nth c)]
-                      i (range)]
+                      rand-char [#(rand-nth c)
+                                 i (range)]]
                   [(rand-nth small-primes)
+                   (rand-nth big-primes)
                    (->> rand-char (repeatedly gen-len) (apply str seed))])]
     (->> (cp/pfor n-proc [p params]
            ; Loop until we have hashed every row, or the total running score is
@@ -49,7 +56,7 @@
              (loop [score 0 seen-hashes {} rows rows]
                (if-let [row (first rows)]
                  (let [hash        (f row)
-                       seen-count  (get seen-hashes hash 0)
+                       seen-count  (int (get seen-hashes hash 0))
                        score       (+ score (case seen-count 0 0 1 2 (- seen-count 1)))
                        seen-hashes (assoc seen-hashes hash (inc seen-count))]
                    (if (< score max-score)
